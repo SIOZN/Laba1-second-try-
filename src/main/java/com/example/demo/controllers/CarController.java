5e04f088f48cbf72c6408d11af8e5d540ea58cd2
SIOZN <145025791+SIOZN@users.noreply.github.com>
2023-09-26 18:53:44 +0700

    Update for CarController

--- src/main/java/com/example/demo/controllers/CarController.java
+++ src/main/java/com/example/demo/controllers/CarController.java
@@ -5,7 +5,6 @@ import com.example.demo.models.Car;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
 
 
@@ -13,6 +12,26 @@ import java.util.List;
 @RestController
 @RequestMapping("api/cars")
 public class CarController {
+    private final List<Car> cars;
 
+    public CarController(List<Car> carList) {
 
+            Car n1 = new Car(1l, "KIA","passenger",2012,true);
+            Car n2 = new Car(2l, "LADA","sedan",2020,false);
+
+            cars = List.of(n1, n2);
+
+    }
+    @GetMapping
+    public List<Car> getCars() {
+        return cars;
+    }
+
+    @GetMapping("/{car_id}")
+    public Car getCar(@PathVariable("car_id") Long carId) {
+        return cars.stream()
+                .filter(car -> car.Id().equals(carId))
+                .findAny()
+                .orElse(null);
+    }
 }
