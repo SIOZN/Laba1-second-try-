6795a9eacd6241ad61afbacfb228cbb6b28e4fac
SIOZN <145025791+SIOZN@users.noreply.github.com>
2023-10-07 18:46:26 +0700

    Add OrganizationController and update CarController

--- src/main/java/com/example/demo/controllers/CarController.java
+++ src/main/java/com/example/demo/controllers/CarController.java
@@ -1,25 +1,25 @@
 package com.example.demo.controllers;
+
 import org.springframework.web.bind.annotation.*;
 import com.example.demo.models.Car;
 
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+
 import java.util.List;
 
 
 
 @RestController
+
 @RequestMapping("api/cars")
 public class CarController {
     private final List<Car> cars;
 
     public CarController(List<Car> carList) {
 
-            Car n1 = new Car(1l, "KIA","passenger",2012,true);
-            Car n2 = new Car(2l, "LADA","sedan",2020,false);
-
-            cars = List.of(n1, n2);
+            Car n1 = new Car(1L, "KIA","passenger",2012,true);
+            Car n2 = new Car(2L, "LADA","sedan",2020,false);
+            Car n3 = new Car(3L, "HONDA","passenger",2009,true);
+            cars = List.of(n1, n2, n3);
 
     }
     @GetMapping
--- src/main/java/com/example/demo/models/Car.java
+++ src/main/java/com/example/demo/models/Car.java
@@ -1,9 +1,11 @@
 package com.example.demo.models;
 
 public record Car(Long Id,
-                  String Brand_Name,
-                  String Type,
+                  String Brand_Name, // make it enum
+                  String Type, // ues camel-case for variable names
                   Integer Year,
                   Boolean Is_Mech
+
+                  // add others date types
 
 ) {}
